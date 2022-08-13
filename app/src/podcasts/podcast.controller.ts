6b969c2b8900645fafbff49498bb0fe5a1b690ec
Victor Georg Oliveira <victor.georg@m4u.com.br>
2022-08-12 21:08:12 -0300

    Added findAll, findOne, BestPodcasts to podcast controller

--- app/src/podcasts/podcast.controller.ts
+++ app/src/podcasts/podcast.controller.ts
@@ -1,12 +1,38 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  DefaultValuePipe,
+  Get,
+  Param,
+  ParseIntPipe,
+  Query,
+} from '@nestjs/common';
+import { Pagination } from 'nestjs-typeorm-paginate';
+import { Podcast } from './podcast.entity';
 import { PodcastService } from './podcast.service';
 
 @Controller()
 export class PodcastController {
   constructor(private readonly podcastService: PodcastService) {}
 
+  @Get('/podcasts')
+  findAll(): Promise<Podcast[]> {
+    return this.podcastService.findAll();
+  }
+
+  @Get('/podcasts/:id')
+  findOne(@Param('id') id: string): Promise<Podcast> {
+    return this.podcastService.findOne(id);
+  }
+
   @Get('/best_podcasts')
-  getBest(): Array<object> {
-    return this.podcastService.getBest();
+  getBest(
+    @Query('page', new DefaultValuePipe(1), ParseIntPipe) page = 1,
+    @Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit = 10,
+  ): Promise<Pagination<Podcast>> {
+    limit = limit > 100 ? 100 : limit;
+    return this.podcastService.paginate({
+      page,
+      limit,
+    });
   }
 }
--- app/src/podcasts/podcast.service.ts
+++ app/src/podcasts/podcast.service.ts
@@ -1,8 +1,29 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import {
+  IPaginationOptions,
+  paginate,
+  Pagination,
+} from 'nestjs-typeorm-paginate';
+import { Repository } from 'typeorm';
+import { Podcast } from './podcast.entity';
 
 @Injectable()
 export class PodcastService {
-  getBest(): Array<object> {
-    return [];
+  constructor(
+    @InjectRepository(Podcast)
+    private podcastsRepository: Repository<Podcast>,
+  ) {}
+
+  findAll(): Promise<Podcast[]> {
+    return this.podcastsRepository.find();
+  }
+
+  findOne(id: string): Promise<Podcast> {
+    return this.podcastsRepository.findOneBy({ id });
+  }
+
+  async paginate(options: IPaginationOptions): Promise<Pagination<Podcast>> {
+    return paginate<Podcast>(this.podcastsRepository, options);
   }
 }
